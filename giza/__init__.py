7547af2ecabecf73da68d91d4883996dd7031374
kamangir <arash@kamangir.net>
2023-01-22 17:04:57 -0800

    digest - kamangir/bolt#741

--- giza/__init__.py
+++ giza/__init__.py
@@ -1,5 +1,5 @@
 NAME = "giza"
 
-VERSION = "2.9.1"
+VERSION = "2.11.1"
 
 from .functions import *
--- giza/functions.py
+++ giza/functions.py
@@ -1,6 +1,9 @@
+import os
+from tqdm import tqdm
 from . import NAME
-from abcli import logging
 from abcli.plugins import list_of_external
+from abcli import file
+from abcli import logging
 import logging
 
 logger = logging.getLogger(__name__)
@@ -8,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 def digest(list_of_repos):
     if list_of_repos == "all":
-        list_of_repos = list_of_external(True)
+        list_of_repos = ["awesome-bash-cli"] + list_of_external(True)
     else:
         list_of_repos = list_of_repos.split("+")
 
@@ -19,5 +22,18 @@ def digest(list_of_repos):
             ", ".join(list_of_repos),
         )
     )
+
+    abcli_path_git = os.getenv("abcli_path_git", "")
+
+    file_count = 0
+    for repo_name in tqdm(list_of_repos):
+        list_of_files = file.list_of(
+            os.path.join(abcli_path_git, repo_name, "*.sh"),
+            recursive=True,
+        )
+
+        file_count += len(list_of_files)
+
+    logger.info(f"processed {file_count} file(s)")
 
     return True
