88717c78e355ce70429fb14518481f47ffa66d67
kamangir <arash@kamangir.net>
2023-11-04 13:31:31 -0700

    pdf-update - kamangir/bolt#420

--- .abcli/update_pdf.sh
+++ .abcli/update_pdf.sh
@@ -6,13 +6,17 @@ function giza_update_pdf() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        abcli_show_usage "giza update_pdf [push,~rm]" \
+        abcli_show_usage "giza update_pdf [~pull,push,~rm]" \
             "update assets/giza.pdf in https://github.com/kamangir/giza/."
         return
     fi
 
+    local do_pull=$(abcli_option_int "$options" pull 1)
     local do_push=$(abcli_option_int "$options" push 0)
     local do_rm=$(abcli_option_int "$options" rm 1)
+
+    [[ "$do_pull" == 1 ]] &&
+        abcli_git giza pull
 
     local filename=$HOME/Downloads/giza.pdf
     if [[ ! -f "$filename" ]]; then
