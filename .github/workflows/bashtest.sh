ccfad833fac45a1342ac15e8ebeafefbdf3ee5e3
kamangir <arash@kamangir.net>
2024-04-07 15:29:19 -0700

    bashtest refactor - kamangir/bolt#746

--- .github/workflows/bashtest.sh
+++ .github/workflows/bashtest.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 function runme() {
+    set -x # verbose-mode
+
     local git_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
     for i in {1..3}; do
         git_root=$(dirname $git_root)
@@ -10,8 +12,9 @@ function runme() {
 
     pushd $git_root >/dev/null
     git clone https://github.com/kamangir/awesome-bash-cli.git
-    ls ./awesome-bash-cli/
     popd >/dev/null
+
+    source $git_root/awesome-bash-cli/bash/abcli.sh
 
     echo "🪄"
     return
