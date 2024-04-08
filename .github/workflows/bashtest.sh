5d64a7e918595a0171c3b0939c08bb98f311ca65
kamangir <arash@kamangir.net>
2024-04-07 17:16:13 -0700

    bashtests refactor - kamangir/bolt#746

--- .github/workflows/bashtest.sh
+++ .github/workflows/bashtest.sh
@@ -7,16 +7,20 @@ function runme() {
     pip3 install -e .
     pip3 install -r requirements.txt
 
-    pushd ../../../ >/dev/null
+    local git_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+    for i in {1..3}; do
+        git_root=$(dirname $git_root)
+    done
+    echo "git_root: $git_root"
 
+    pushd $git_root >/dev/null
     git clone https://github.com/kamangir/awesome-bash-cli.git
-
     cd awesome-bash-cli
     pip3 install -e .
     pip3 install -r requirements.txt
-    source ./bash/abcli.sh ~terraform
-
     popd >/dev/null
+
+    source $git_root/awesome-bash-cli/bash/abcli.sh ~terraform
 
     giza test "$@"
 
