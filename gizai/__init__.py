bb9640cab4aa4fa01e79d50159528c980937ea2f
kamangir <arash@kamangir.net>
2024-05-20 21:13:56 -0700

    blueness update - kamangir/bolt#746

--- gizai/__init__.py
+++ gizai/__init__.py
@@ -4,4 +4,4 @@ ICON = "🔻"
 
 DESCRIPTION = f"{ICON} a recipe for AI languages."
 
-VERSION = "5.28.1"
+VERSION = "5.29.1"
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from blueness.pypi import setup
 
 setup(
     filename=__file__,
+    repo_name="giza",
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
