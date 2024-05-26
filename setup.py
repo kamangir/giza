5eb2605354d984739dd3f10de4da8dd3189ae83b
kamangir <arash@kamangir.net>
2024-05-26 10:04:38 -0700

    build refactor - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from gizai import NAME, VERSION, DESCRIPTION
+from gizai import NAME, VERSION, DESCRIPTION, REPO_NAME
 from blueness.pypi import setup
 
 
 setup(
     filename=__file__,
-    repo_name="giza",
+    repo_name=REPO_NAME,
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
