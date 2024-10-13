845ce1639af83ca86f536f31e2c18db8db5d052d
kamangir <arash@kamangir.net>
2024-10-13 13:15:36 -0700

    git refactor - kamangir/bolt#746

--- gizai/.abcli/actions.sh
+++ gizai/.abcli/actions.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 function giza_action_git_before_push() {
-    [[ "$(abcli_git get_branch)" == "main" ]] &&
-        giza pypi build
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
+        return 0
+
+    giza pypi build
 }
--- gizai/__init__.py
+++ gizai/__init__.py
@@ -4,7 +4,7 @@ ICON = "🔻"
 
 DESCRIPTION = f"{ICON} a recipe for AI languages."
 
-VERSION = "5.114.1"
+VERSION = "5.115.1"
 
 REPO_NAME = "giza"
 
