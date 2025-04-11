c71aa2aa0e19aa978cdddd2ef6725322015cc7bb
kamangir <arash@kamangir.net>
2025-04-11 12:58:49 -0700

    start of refactors-2025-04-11-4vg8v3 🌀 - kamangir/bolt#746

--- gizai/.abcli/giza.sh
+++ gizai/.abcli/giza.sh
@@ -4,7 +4,7 @@ function giza() {
     local task=$1
     [[ "$task" == "open" ]] && task="open_pdf"
 
-    abcli_generic_task \
+    bluer_ai_generic_task \
         plugin=giza,task=$task \
         "${@:2}"
 }
--- gizai/.abcli/tests/version.sh
+++ gizai/.abcli/tests/version.sh
@@ -4,7 +4,7 @@ function test_giza_version() {
     local options=$1
     local do_dryrun=$(abcli_option_int "$options" dryrun 0)
 
-    abcli_eval dryrun=$do_dryrun \
+    bluer_ai_eval dryrun=$do_dryrun \
         "giza version ${@:2}"
 
     return 0
